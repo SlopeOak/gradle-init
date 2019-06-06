25bf44973efead68006c6b70013898a46f1c5a6e
Stuart Sinclair <aeternuscreperum@gmail.com>
2019-06-06 20:58:57 +0100

    Initializing the gradle init project.

---
+++ src/main/groovy/com/github/slopeoak/GradleInit.groovy
@@ -0,0 +1,11 @@
+package com.github.slopeoak
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class GradleInit implements Plugin<Project> {
+
+    @Override
+    void apply(Project target) {
+    }
+}
---
+++ src/readme.md
@@ -0,0 +1,2 @@
+# Gradle init
+An alternative approach to the official gradle init script.
